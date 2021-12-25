7eca84c0334c89c73c7e26848fccc0d2e6d007b8
LimDongHee <32934089+DongHeeLim@users.noreply.github.com>
2021-12-26 01:31:56 +0900

    .

--- Remote_LED/Remote_LED.ino
+++ Remote_LED/Remote_LED.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LED_PIN 15
 
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(0, 5000, 8);
+  ledcAttachPin(LED_PIN, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for(int i=0;i<=255; i++){
+    ledcWrite(0, i);
+    Serial.println(i);
+    delay(100);
+  }
 }

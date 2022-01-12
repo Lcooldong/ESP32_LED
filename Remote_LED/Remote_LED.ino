a30eae6082845ed39081196f9ddae84f5b51c229
LimDongHee <32934089+DongHeeLim@users.noreply.github.com>
2022-01-12 22:55:08 +0900

    Update Remote_LED.ino

--- Remote_LED/Remote_LED.ino
+++ Remote_LED/Remote_LED.ino
@@ -1,14 +1,26 @@
 #define LED_PIN 15
+#define LED 34
 
 
 void setup() {
   Serial.begin(115200);
   ledcSetup(0, 5000, 8);
   ledcAttachPin(LED_PIN, 0);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  for(int i=0;i<=255; i++){
+  digitalWrite(LED, HIGH);
+  Serial.println("HIGH");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("LOW");
+  delay(1000);
+}
+
+void led_control(){
+  for(int i=0;i<=255; i++)
+  {
     ledcWrite(0, i);
     Serial.println(i);
     delay(100);

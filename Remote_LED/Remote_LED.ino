515d4334b0afd3f60d93402a1f13b0327dde1173
LimDongHee <32934089+DongHeeLim@users.noreply.github.com>
2022-01-14 05:07:32 +0900

    Update Remote_LED.ino

--- Remote_LED/Remote_LED.ino
+++ Remote_LED/Remote_LED.ino
@@ -1,47 +1,42 @@
-#include <Math.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
 #define LED_PIN 15
 #define LED 22
 #define batteryPin 34
+
+String message = "";
+char incomingChar;
 
 int sensorValue = 0;
 float calibration = 0.2;
 int bat_percentage;
 
+int ledState = LOW;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+unsigned long previousMillis2 = 0;
+const long interval2 = 500;
+
+
 void setup() {
   Serial.begin(115200);
+  SerialBT.begin("ESP_bl"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
   ledcSetup(0, 5000, 8);
   ledcAttachPin(LED_PIN, 0);
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(1000);
-  sensorValue = analogRead(batteryPin);
-  Serial.print("sensor : ");
-  Serial.println(sensorValue);
-  
-  float voltage = (((sensorValue * 3.3) / 4095) * 2 + calibration);
-  
-  Serial.print("VOLT : ");
-  Serial.print(voltage);
-  Serial.println("V"); 
-
-  Serial.println(voltage * 10);
-  bat_percentage = (float)map((int)(voltage * 100), 280, 420, 0, 100);
-  
-  if (bat_percentage >= 100){
-    bat_percentage = 100;  
-  }
-  if (bat_percentage <= 0){
-    bat_percentage = 1;
-  }
-  Serial.print("percentage : ");
-  Serial.print(bat_percentage);
-  Serial.println("%");
+  blink();
+  batteryCheck();
 }
+
+
 
 void led_control(){
   for(int i=0;i<=255; i++)
@@ -50,4 +45,46 @@ void led_control(){
     Serial.println(i);
     delay(100);
   }
+}
+
+void blink(){
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    if (ledState == LOW){
+      ledState = HIGH;  
+    }else{
+      ledState = LOW;  
+    }
+    digitalWrite(LED, ledState);
+  }  
+}
+
+void batteryCheck(){
+  unsigned long currentMillis2 = millis();
+  if (currentMillis2 - previousMillis2 >= interval2){
+    previousMillis2 = currentMillis2;
+    sensorValue = analogRead(batteryPin);
+    Serial.print("sensor : ");
+    Serial.println(sensorValue);
+  
+    float voltage = (((sensorValue * 3.3) / 4095) * 2 + calibration);
+  
+    Serial.print("VOLT : ");
+    Serial.print(voltage);
+    Serial.println("V"); 
+
+    
+    bat_percentage = (float)map((int)(voltage * 1000), 2500, 4200, 0, 100);
+  
+    if (bat_percentage >= 100){
+      bat_percentage = 100;  
+    }
+    if (bat_percentage <= 0){
+      bat_percentage = 1;
+    }
+    Serial.print("percentage : ");
+    Serial.print(bat_percentage);
+    Serial.println("%");
+  } 
 }

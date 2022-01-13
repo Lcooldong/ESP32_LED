3b91753a33a145065bfd74d41e28b0a6a26f78e4
LimDongHee <32934089+DongHeeLim@users.noreply.github.com>
2022-01-13 20:41:31 +0900

    Update Remote_LED.ino

--- Remote_LED/Remote_LED.ino
+++ Remote_LED/Remote_LED.ino
@@ -1,6 +1,11 @@
+#include <Math.h>
 #define LED_PIN 15
-#define LED 34
+#define LED 22
+#define batteryPin 34
 
+int sensorValue = 0;
+float calibration = 0.2;
+int bat_percentage;
 
 void setup() {
   Serial.begin(115200);
@@ -11,11 +16,31 @@ void setup() {
 
 void loop() {
   digitalWrite(LED, HIGH);
-  Serial.println("HIGH");
   delay(1000);
   digitalWrite(LED, LOW);
-  Serial.println("LOW");
   delay(1000);
+  sensorValue = analogRead(batteryPin);
+  Serial.print("sensor : ");
+  Serial.println(sensorValue);
+  
+  float voltage = (((sensorValue * 3.3) / 4095) * 2 + calibration);
+  
+  Serial.print("VOLT : ");
+  Serial.print(voltage);
+  Serial.println("V"); 
+
+  Serial.println(voltage * 10);
+  bat_percentage = (float)map((int)(voltage * 100), 280, 420, 0, 100);
+  
+  if (bat_percentage >= 100){
+    bat_percentage = 100;  
+  }
+  if (bat_percentage <= 0){
+    bat_percentage = 1;
+  }
+  Serial.print("percentage : ");
+  Serial.print(bat_percentage);
+  Serial.println("%");
 }
 
 void led_control(){

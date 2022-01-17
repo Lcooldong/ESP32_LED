5f18b3c8bf40e9c911da338063b5ed0ea09b0cb7
LimDongHee <32934089+DongHeeLim@users.noreply.github.com>
2022-01-17 16:41:27 +0900

    Update Remote_LED.ino

--- Remote_LED/Remote_LED.ino
+++ Remote_LED/Remote_LED.ino
@@ -13,18 +13,21 @@ char incomingChar;
 
 int sensorValue = 0;
 float calibration = 0.2;
+float voltage;
 int bat_percentage;
 
+int blink_switch = 0;
 int ledState = LOW;
 unsigned long previousMillis = 0;
 const long interval = 1000;
 unsigned long previousMillis2 = 0;
-const long interval2 = 500;
+const long interval2 = 2000;
 
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP_bl"); //Bluetooth device name
+  SerialBT.begin("LED_Remote"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
   ledcSetup(0, 5000, 8);
   ledcAttachPin(LED_PIN, 0);
@@ -32,8 +35,47 @@ void setup() {
 }
 
 void loop() {
-  blink();
+  // 시리얼 쓴 것 -> 블루투스로 전송
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+    
+    
+  }
+  
+  // 블루투스 받아온 것 -> 시리얼에 쓰기
+  // Read received messages (LED control command)
+  if (SerialBT.available()){
+    char incomingChar = SerialBT.read();
+    if (incomingChar != '\n'){    // 개행 문자로 안끝나면 계속 추가
+      message += String(incomingChar);
+    }
+    else{
+      message = ""; // 개행문자면 끝
+    }
+    Serial.write(incomingChar);  // 받은 것 계속 쓰기
+  }
+  // Check received message and control output accordingly
+  if (message =="led_on"){
+    digitalWrite(LED, HIGH);
+    //Serial.println("Turn ON");
+  }
+  else if (message =="led_off"){
+    digitalWrite(LED, LOW);
+    //Serial.println("Turn OFF");
+  }else if (message == "blink_on"){
+    blink_switch = 1;
+  }else if (message == "blink_off"){
+    digitalWrite(LED, LOW);
+    blink_switch = 0;
+  }
+
+  if (blink_switch == 1){
+    blink_led();
+  }
+  
+  delay(20);
   batteryCheck();
+  SerialBT.println(String(voltage) + "," + String(bat_percentage) + ",");
 }
 
 
@@ -47,7 +89,7 @@ void led_control(){
   }
 }
 
-void blink(){
+void blink_led(){
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval){
     previousMillis = currentMillis;
@@ -57,7 +99,7 @@ void blink(){
       ledState = LOW;  
     }
     digitalWrite(LED, ledState);
-  }  
+  }
 }
 
 void batteryCheck(){
@@ -65,14 +107,14 @@ void batteryCheck(){
   if (currentMillis2 - previousMillis2 >= interval2){
     previousMillis2 = currentMillis2;
     sensorValue = analogRead(batteryPin);
-    Serial.print("sensor : ");
-    Serial.println(sensorValue);
+    //Serial.print("sensor : ");
+    //Serial.println(sensorValue);
   
-    float voltage = (((sensorValue * 3.3) / 4095) * 2 + calibration);
+    voltage = (((sensorValue * 3.3) / 4095) * 2 + calibration);
   
-    Serial.print("VOLT : ");
-    Serial.print(voltage);
-    Serial.println("V"); 
+    //Serial.print("VOLT : ");
+    //Serial.print(voltage);
+    //Serial.println("V"); 
 
     
     bat_percentage = (float)map((int)(voltage * 1000), 2500, 4200, 0, 100);
@@ -83,8 +125,8 @@ void batteryCheck(){
     if (bat_percentage <= 0){
       bat_percentage = 1;
     }
-    Serial.print("percentage : ");
-    Serial.print(bat_percentage);
-    Serial.println("%");
+    //Serial.print("percentage : ");
+    //Serial.print(bat_percentage);
+    //Serial.println("%");
   } 
 }
